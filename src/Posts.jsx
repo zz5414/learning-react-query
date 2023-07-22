cfa4153598871608793811761dfc0d559c561e8c
heejin.park <zzqq5414@gmail.com>
2023-07-22 23:31:17 +0900

    There are some error!! can't prefetching

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { useQuery, QueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,10 +15,21 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = new QueryClient();
+
+  useEffect(() => {
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
   const { data, isError, error, isLoading } = useQuery(
     ["posts", currentPage],
-    () => fetchPosts(currentPage)
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      // keepPreviousData: true,
+    }
   );
 
   if (isLoading) return <h3>Loading...</h3>;
