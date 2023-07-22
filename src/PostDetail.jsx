0dfcb5eba62c3dfecd8c99f1502c21a9309fed2e
heejin.park <zzqq5414@gmail.com>
2023-07-22 19:05:12 +0900

    fetch comments - React query에서 파라미터를 주는 방법 단순히 파라미터를 주는 것이 아니라 query key를 다르게 주어 파라미터마다 cache 되도록

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,24 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+
+  if (isError)
+    return (
+      <>
+        <h3>Error!</h3>
+        <div>{error}</div>
+      </>
+    );
+  if (isLoading)
+    return (
+      <>
+        <h3> Loading... </h3>
+      </>
+    );
 
   return (
     <>
